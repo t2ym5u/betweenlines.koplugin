e4e9b35d8871fb0fa85e8be36967f092ea01dc7d
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-17 08:56:15 +0200

    chore: bump version to 1.1.2

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "betweenlines",
     fullname    = _("Between Lines Sudoku"),
     description = _("Sudoku with between-lines constraints"),
-    version     = "1.1.1",
+    version     = "1.1.2",
 }
