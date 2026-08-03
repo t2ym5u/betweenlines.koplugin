ad9dc845f60361903d030429020b760b0ad73fd3
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-03 15:49:56 +0200

    chore: bump version to 1.1.11

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Between Lines Sudoku"),
     description = _("Sudoku with between-lines constraints"),
-    version     = "1.1.10",
+    version     = "1.1.11",
 }
