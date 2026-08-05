edc4ae04b381e7e18ff102ad2808de2b028c0798
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:58:52 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Between Lines Sudoku"] = { fr = "Sudoku Entre Lignes" },
-    ["Sudoku with between-lines constraints"] = { fr = "Sudoku avec contraintes entre les lignes" },
+    ["Between Lines Sudoku"] = { fr = "Sudoku Entre Lignes", es = "Sudoku Entre Líneas", de = "Sudoku Zwischen Linien" },
+    ["Sudoku with between-lines constraints"] = { fr = "Sudoku avec contraintes entre les lignes", es = "Sudoku con restricciones entre líneas", de = "Sudoku mit Zwischen-Linien-Beschränkungen" },
 }
